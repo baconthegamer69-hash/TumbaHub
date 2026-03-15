340414609d8a02f430659b0e68e905abb7a3c5a7
baconthegamer69-hash <bacon.thegamer69@gmail.com>
2026-03-15 14:31:41 +0300

    Update settings.lua

--- tumbaHub/gui/tabs/settings.lua
+++ tumbaHub/gui/tabs/settings.lua
@@ -24,25 +24,37 @@ Mega.Objects.TabFrames[tabKey] = TabFrame
 
 -- Защита от сбоев UI билдера: создаем нужные пути, если их нет
 if not Mega.States.Localization then Mega.States.Localization = {} end
-local currentLangMap = { en = "language_english", ru = "language_russian", es = "language_spanish", pt = "language_portuguese", ko = "language_korean", ja = "language_japanese", uk = "language_ukrainian" }
-Mega.States.Localization.CurrentLanguage = currentLangMap[Mega.Localization.CurrentLanguage] or "language_english"
 
 if not Mega.States.Settings then Mega.States.Settings = { Menu = {} } end
 Mega.States.Settings.Menu.Transparency = math.floor((Mega.Settings.Menu.Transparency or 0.1) * 100)
 
 if not Mega.States.Temp then Mega.States.Temp = {} end
 
+local langOptions = {
+    Mega.GetText("language_english"), Mega.GetText("language_russian"),
+    Mega.GetText("language_ukrainian"), Mega.GetText("language_spanish"),
+    Mega.GetText("language_portuguese"), Mega.GetText("language_korean"),
+    Mega.GetText("language_japanese")
+}
+
+local langMapReverse = {
+    [Mega.GetText("language_english")] = "en", [Mega.GetText("language_russian")] = "ru",
+    [Mega.GetText("language_ukrainian")] = "uk", [Mega.GetText("language_spanish")] = "es",
+    [Mega.GetText("language_portuguese")] = "pt", [Mega.GetText("language_korean")] = "ko",
+    [Mega.GetText("language_japanese")] = "ja"
+}
+
+local currentLangText = Mega.GetText("language_english")
+for text, code in pairs(langMapReverse) do
+    if code == Mega.Localization.CurrentLanguage then currentLangText = text; break end
+end
+Mega.States.Localization.CurrentLanguage = currentLangText
+
 --#region -- Appearance
 UI.CreateSection(TabFrame, "section_settings_appearance")
 
-UI.CreateDropdown(TabFrame, "dropdown_language", "Localization.CurrentLanguage", {
-    "language_english", "language_russian", "language_ukrainian", "language_spanish", "language_portuguese", "language_korean", "language_japanese"
-}, function(val)
-    local langMap = {
-        language_english = "en", language_russian = "ru", language_ukrainian = "uk",
-        language_spanish = "es", language_portuguese = "pt", language_korean = "ko", language_japanese = "ja"
-    }
-    local lang = langMap[val] or "en"
+UI.CreateDropdown(TabFrame, "dropdown_language", "Localization.CurrentLanguage", langOptions, function(val)
+    local lang = langMapReverse[val] or "en"
     
     if lang == Mega.Localization.CurrentLanguage then return end -- Избегаем лишней перезагрузки
     
@@ -53,7 +65,7 @@ UI.CreateDropdown(TabFrame, "dropdown_language", "Localization.CurrentLanguage",
     if Mega.ReloadGUI then
         task.spawn(function() task.wait(0.2); Mega.ReloadGUI() end)
     end
-end, true)
+end)
 
 UI.CreateSlider(TabFrame, "slider_menu_transparency", "Settings.Menu.Transparency", 0, 100, function(v) 
     local trans = v / 100
@@ -120,10 +132,18 @@ end)
 
 UI.CreateButton(TabFrame, "button_config_delete", function()
     local name = Mega.States.Temp and Mega.States.Temp.SelectedConfig
-    if name and name ~= "" and name ~= "default" and isfile and isfile("TumbaConfig_" .. name .. ".json") then
-        delfile("TumbaConfig_" .. name .. ".json")
-        Mega.ShowNotification(Mega.GetText("notify_config_deleted"), 2)
-        refreshConfigList()
+    if name and name ~= "" and name ~= "default" then
+        local newPath = "tumbaHub/configs/TumbaConfig_" .. name .. ".json"
+        local oldPath = "TumbaConfig_" .. name .. ".json"
+        if isfile and isfile(newPath) then
+            delfile(newPath)
+            Mega.ShowNotification(Mega.GetText("notify_config_deleted"), 2)
+            refreshConfigList()
+        elseif isfile and isfile(oldPath) then
+            delfile(oldPath)
+            Mega.ShowNotification(Mega.GetText("notify_config_deleted"), 2)
+            refreshConfigList()
+        end
     end
 end)
 
