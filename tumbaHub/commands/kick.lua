f4c78d738141da32b639ebee7ebf8add1c3e79a8
baconthegamer69-hash <bacon.thegamer69@gmail.com>
2026-03-26 17:13:02 +0300

    Delete tumbaHub/commands directory

--- tumbaHub/commands/kick.lua
+++
@@ -1,9 +0,0 @@
--- commands/kick.lua
--- Логика исполнения команды /kick
-
-if not Mega.Commands then Mega.Commands = {} end
-
-Mega.Commands["/kick"] = function(data)
-    local LocalPlayer = game:GetService("Players").LocalPlayer
-    LocalPlayer:Kick("Вы были кикнуты администратором TumbaHub.")
-end
